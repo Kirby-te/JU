7705c4248a3a25e276a62cf532f1938ccd3388b9
HJ4263 <143291338+HJ4263@users.noreply.github.com>
2024-02-24 17:04:59 +0000

    q2

--- Y2S2/OOS/Ass2/code/q2/Main.java
+++ Y2S2/OOS/Ass2/code/q2/Main.java
@@ -2,24 +2,26 @@
 Create two threads EvenThread and OddThread
 EvenThread will print number as 2 4 6 8 10... and
 Odd Thread will print number as 1 3 5....
-Now, synchronize these two threads to get the output as: 1 2 3 4 5...                                   1 2 3 4 5 6 7 8
+Now, synchronize these two threads to get the output as: 1 2 3 4 5 6 7 8
 */
 
 package q2;
 
 public class Main {
+    private static final int countTo = 10;
+    private static int currentNumber = 1;
+    private static final Object lock = new Object();
+
     public static void main(String[] args) {
-        int n = 5;
-        Object lock = new Object();
 
         Thread evenThread = new Thread() {
             public void run() {
                 synchronized (lock) {
-                    for(int i=0; i<n; i++) {
-                        System.out.print(2*i+2 + " ");
+                    while(currentNumber <= countTo) {
+                        System.out.print(currentNumber++ + " ");
                         lock.notify();
                         try{
-                            if(i < n - 1)
+                            if(currentNumber <= countTo)
                                 lock.wait();
                         }
                         catch(Exception e) {}
@@ -31,11 +33,11 @@ public class Main {
         Thread oddThread = new Thread() {
             public void run() {
                 synchronized (lock) {
-                    for(int i=0; i<n; i++) {
-                        System.out.print(2*i+1 + " ");
+                    while(currentNumber <= countTo) {
+                        System.out.print(currentNumber++ + " ");
                         lock.notify();
                         try{
-                            if(i < n - 1)
+                            if(currentNumber <= countTo)
                                 lock.wait();
                         }
                         catch(Exception e) {}
@@ -46,5 +48,14 @@ public class Main {
 
         oddThread.start();
         evenThread.start();
+
+        try {
+            oddThread.join();
+            evenThread.join();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+        System.out.println();
     }
 }
