f5e41cf16c4a1370c2a60738fcacf107a52d3785
AK <143291338+AK-mist@users.noreply.github.com>
2024-02-25 00:52:13 +0530

    Update Main.java

--- Y2S2/OOS/Ass2/code/q6/Main.java
+++ Y2S2/OOS/Ass2/code/q6/Main.java
@@ -9,6 +9,62 @@
 
 package q6;
 
-public class Main {
-    
+public class Search {
+    private static int n;
+    private static int numberOfThread;
+    private static Thread[] threads;
+    private static int threadIndexOfElement = -1;
+    private static int arrayIndexOfTarget = -1;
+    private static int currentThreadIndex;
+
+    public static int binarySearch(int[] arr, int startIndex, int endIndex, int target) {
+        while(startIndex <= endIndex) {
+            int midIndex = startIndex + (endIndex - startIndex) / 2;
+            if(target < arr[midIndex]) {
+                endIndex = midIndex - 1;
+            } else if(target > arr[midIndex]) {
+                startIndex = midIndex + 1;
+            } else {
+                return midIndex;
+            }
+        }
+        return -1;
+    }
+
+    public static int[] searchElementUsingThreads(int[] arr, int target) {
+        n = arr.length;
+        numberOfThread = (n / 10) + (n % 10);
+        threads = new Thread[numberOfThread];
+        currentThreadIndex = 0;
+
+        while(currentThreadIndex < numberOfThread) {
+            int startIndex = (currentThreadIndex)*10;
+            int endIndex = Math.min((currentThreadIndex+1)*10 - 1, n - 1);
+            threads[currentThreadIndex] = new Thread() {
+                public void run() {
+                    int index = binarySearch(arr, startIndex, endIndex, target);
+                    if(index != -1) {
+                        threadIndexOfElement = currentThreadIndex + 1;
+                        arrayIndexOfTarget = currentThreadIndex * 10 + index + 1;
+                    }
+                }
+            };
+            currentThreadIndex++;
+        }
+
+        for(int i=0; i<numberOfThread; i++) {
+            threads[i].run();
+        }
+
+        try {
+            for(int i=0; i<numberOfThread; i++) {
+                threads[i].join();
+            }
+        }
+        catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+        return new int[] {threadIndexOfElement, arrayIndexOfTarget};// if not found: {-1, -1} 
+    }
 }
