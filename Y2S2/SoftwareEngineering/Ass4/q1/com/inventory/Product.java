9dc93624c64f863a25f922640122ee90f3dd9808
kaji <manirul.kaji78@gmail.com>
2024-03-02 19:18:44 +0530

    added: com.inventory.Product.java

---
+++ Y2S2/SoftwareEngineering/Ass4/q1/com/inventory/Product.java
@@ -0,0 +1,72 @@
+package com.inventory;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Product {
+    /*
+     * for productId the identifierIndex = 0
+     * for productname the identfierIndex = 1 
+    */
+    public boolean isAvailable(String productDetail, int identifierIndex) {
+        try(BufferedReader br = new BufferedReader(new FileReader("./database/inventory.csv"))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] products = line.split(", ");
+                if(productDetail.equals(products[identifierIndex])) {
+                    return true;
+                }
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Need inventory database.");
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        }
+
+        return false;
+    }
+
+    public double getCost(String productDetail, int identifierIndex) {
+        try(BufferedReader br = new BufferedReader(new FileReader("./database/inventory.csv"))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] products = line.split(", ");
+                if(productDetail.equals(products[identifierIndex])) {
+                    return Double.parseDouble(products[2]);
+                }
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Need inventory database.");
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        }
+
+        return -1.0;
+    }
+
+    public int availableQuantity(String productDetail, int identifierIndex) {
+        try(BufferedReader br = new BufferedReader(new FileReader("./database/inventory.csv"))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] products = line.split(", ");
+                if(productDetail.equals(products[identifierIndex])) {
+                    return Integer.parseInt(products[3]);
+                }
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Need inventory database.");
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        }
+
+        return 0;
+    }
+}
