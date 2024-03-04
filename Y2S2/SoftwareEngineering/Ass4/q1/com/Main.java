22fc9043e0fe884acf9738c77bde90df0a80247c
kaji <manirul.kaji78@gmail.com>
2024-03-04 23:57:40 +0530

    Main.java define Goals

--- Y2S2/SoftwareEngineering/Ass4/q1/com/Main.java
+++ Y2S2/SoftwareEngineering/Ass4/q1/com/Main.java
@@ -2,6 +2,42 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
+        // who are u (customer/ seller)
+
+        // seller
+
+            // product list
+
+            // add/remove product
+
+            // modify existing product
+
+                // add/removes items
+
+                // modify cost
+            
+            // purchase history (all)
+
+            // customer info (all)
         
+        // customer
+
+            // product list (except quantity)
+
+            // login
+
+                // if exists do nothing else create
+
+            // profile
+
+            // purchase history (own)
+
+            // buy product
+
+                // choose quantity
+
+                // pay
+
+                    // check purchase status
     }
 }
