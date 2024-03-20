ac61994c47582171690d8dde8268737cbed6b299
kaleshray <158251775+kaleshray@users.noreply.github.com>
2024-03-20 11:53:19 +0530

    Create Emp.java

---
+++ Y2S2/OOS/Ass3/test/Emp.java
@@ -0,0 +1,48 @@
+package test;
+
+import java.io.*;
+import java.util.*;
+
+public class Emp{
+  private int id;
+  private double height;
+  private String name;
+  
+  enum Name {
+    Kirito, Alice, Bob;
+  }
+  
+  @interface Inter1{}
+
+  public Emp(int id, double height, String name){
+    super();
+    this.id = id;
+    this.height = height;
+    this.name = name;
+  }
+
+  Emp(){}
+  
+  public int geIid(){
+    return id;
+  }
+  
+  public void setId(int id, int num){
+    this.id = id;
+  }
+
+  public double getHeight(){
+    return height;
+  }
+
+  public String getName(){
+    return name;
+  }
+  
+  public void setName(String name){
+    this.name = name;
+  }
+
+  class A{}
+  class B{}
+}
