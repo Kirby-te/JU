d6f987dd6df47724d4f09fc0fc6a9ef35a3714ac
kaji <manirul.kaji78@gmail.com>
2024-04-02 17:29:23 +0530

    grade table added

--- Y2S2/SoftwareEngineering/Ass4/marks_management/college.py
+++ Y2S2/SoftwareEngineering/Ass4/marks_management/college.py
@@ -29,3 +29,35 @@ class SubjectDB:
     def close_connection(self):
         self.connection.close()
         
+
+
+class GradeDB:
+    def __init__(self, db_path='./database/college.db'):
+        self.db_path = db_path
+        self.connection = sqlite3.connect(self.db_path)
+        self.cursor = self.connection.cursor()
+        self.create_table()
+    
+    def create_table(self):
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS
+            grades
+            (
+                gradeId INTEGER PRIMARY KEY,
+                grade TEXT UNIQUE NOT NULL,
+                lower_bound INTEGER UNIQUE NOT NULL,
+                upper_bound INTEGER UNIQUE NOT NULL
+            )
+            """)
+        self.connection.commit()
+    
+    def add_grade(self, grade, lower_bound, upper_bound):
+        try:
+            self.cursor.execute("INSERT INTO grades (grade, lower_bound, upper_bound) VALUES (?, ?, ?)",
+                                (grade, lower_bound, upper_bound))
+            self.connection.commit()
+            print("Grade added successfully.")
+        except sqlite3.IntegrityError:
+            print("Grade already exists. Please provide unique details.")
+            
+    def close_connection(self):
+        self.connection.close()
